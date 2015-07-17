692a2db3d047c5dc855bfccf92749d948ce65b66
Schuyler St. Leger <schuyler.st.leger@gmail.com>
2015-07-16 22:19:14 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+from subprocess import call
+from sys import version_info
+
+def get_input(prompt):
+    if version_info < (3,0):
+        return raw_input(prompt)
+    else:
+        return input(prompt)
+
+cfile = get_input("Enter C file name: ")
+afile = get_input("Enter Assembly output file name: ")
+
+print("Compiling to Assembly...")
+
+call(["gcc", "-S", "-O0", cfile, "-o", afile])
---
+++ test.c
@@ -0,0 +1,11 @@
+int x = 94;
+
+void func(){
+    int a = 40;
+    int b = a+2;    
+}
+
+int main(void){
+    func();
+    return 0;
+}
