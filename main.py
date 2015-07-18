597ef2616e55526cbd34c14bfe2764bf805df59a
Schuyler St. Leger <schuyler.st.leger@gmail.com>
2015-07-17 23:08:54 -0700

    Add 0x84 to main python script

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from __future__ import print_function
 from subprocess import call
 from sys import version_info
+import curses
+import curses.textpad
+from time import sleep
+
 
 def get_input(prompt):
     if version_info < (3, 0):
@@ -14,3 +18,19 @@ afile = get_input("Enter Assembly output file name: ")
 print("Compiling to Assembly...")
 
 call(["gcc", "-S", "-O0", cfile, "-o", afile])
+
+stdscr = curses.initscr()
+curses.start_color()
+curses.noecho()
+curses.cbreak()
+stdscr.keypad(1)
+
+stdscr.addstr("teststr")
+stdscr.refresh()
+
+sleep(1)
+
+curses.nocbreak()
+stdscr.keypad(0)
+curses.echo()
+curses.endwin()
