9d8c6858961c44405c6889f374df3e95a0123e92
Schuyler St. Leger <schuyler.st.leger@gmail.com>
2015-07-17 23:15:03 -0700

    Add example code

---
+++ code/for-down.c
@@ -0,0 +1,7 @@
+int main(void) {
+    unsigned char x = 0;
+    unsigned char i;
+    for(i = 10; i > 0; i--) {
+        x ++;
+    }
+}
---
+++ code/for-up.c
@@ -0,0 +1,7 @@
+int main(void) {
+    unsigned char x = 0;
+    unsigned char i;
+    for(i = 1; i < 10; i++) {
+        x ++;
+    }
+}
